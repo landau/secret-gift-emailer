26a14de7c876b937cf37299ef7b36c2b8e8cef60
Landau <landau@users.noreply.github.com>
2020-12-07 10:20:17 -0500

    chore: refactor code clean up

--- main.go
+++ main.go
@@ -13,14 +13,7 @@ import (
 	"time"
 )
 
-type person struct {
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	Address string `json:"address"`
-}
-
-func getTemplate(p person) (string, error) {
-	body := `
+const body = `
 <h1>Happy Hanukkah!</h1>
 <br />
 <h2>Here is your Secret Hanukkah Exchannge receipient:</h2>
@@ -34,10 +27,31 @@ Address: {{.Address}}
 Please send a gift in the range of $20 by Dec 17th.
 `
 
-	t := template.Must(template.New("email").Parse(body))
+var emailTemplate = template.Must(template.New("email").Parse(body))
 
+func createEmailHeaders(from, to, subject string) string {
+	headers := make(map[string]string)
+
+	headers["From"] = from
+	headers["To"] = to
+	headers["Subject"] = subject
+
+	headers["MIME-Version"] = "1.0"
+	headers["Content-Type"] = fmt.Sprintf("%s; charset=\"utf-8\"", "text/html")
+	headers["Content-Transfer-Encoding"] = "quoted-printable"
+	headers["Content-Disposition"] = "inline"
+
+	headerMessage := ""
+	for key, value := range headers {
+		headerMessage += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	return headerMessage
+}
+
+func parseTemplate(t *template.Template, data interface{}) (string, error) {
 	var output bytes.Buffer
-	err := t.Execute(&output, p)
+	err := t.Execute(&output, data)
 
 	if err != nil {
 		log.Println("executing template:", err)
@@ -47,43 +61,36 @@ Please send a gift in the range of $20 by Dec 17th.
 	return output.String(), nil
 }
 
-func sendMail(fromPerson, toPerson person, sender, password string) {
-	host := "smtp.gmail.com:587"
-	auth := smtp.PlainAuth("", sender, password, "smtp.gmail.com")
+func createEmailBody(body string) string {
+	var buf bytes.Buffer
+	quotedWriter := quotedprintable.NewWriter(&buf)
+	quotedWriter.Write([]byte(body))
+	quotedWriter.Close()
+	return buf.String()
+}
 
-	header := make(map[string]string)
-	header["From"] = sender
-	header["To"] = fromPerson.Email
-	header["Subject"] = "Secret Hanukkah Gift Exchange"
+type emailConfig struct {
+	ToEmail   string
+	FromEmail string
+	Subject   string
+	Body      string
+	Person    person // The person to be templated in the body of the email
+	Password  string
+}
 
-	header["MIME-Version"] = "1.0"
-	header["Content-Type"] = fmt.Sprintf("%s; charset=\"utf-8\"", "text/html")
-	header["Content-Transfer-Encoding"] = "quoted-printable"
-	header["Content-Disposition"] = "inline"
+func sendGMail(conf emailConfig) error {
+	host := "smtp.gmail.com"
+	auth := smtp.PlainAuth("", conf.FromEmail, conf.Password, host)
 
-	headerMessage := ""
-	for key, value := range header {
-		headerMessage += fmt.Sprintf("%s: %s\r\n", key, value)
-	}
+	addr := host + ":587"
+	to := []string{conf.ToEmail}
 
-	body, err := getTemplate(toPerson)
-	if err != nil {
-		log.Printf("Error parsing template: %s", err)
-	}
+	headers := createEmailHeaders(conf.FromEmail, conf.ToEmail, conf.Subject)
+	body := createEmailBody(conf.Body)
+	msg := []byte(headers + "\r\n" + body)
 
-	var bodyMessage bytes.Buffer
-	temp := quotedprintable.NewWriter(&bodyMessage)
-	temp.Write([]byte(body))
-	temp.Close()
-
-	finalMessage := headerMessage + "\r\n" + bodyMessage.String()
-	status := smtp.SendMail(host, auth, sender, []string{fromPerson.Email}, []byte(finalMessage))
-
-	if status != nil {
-		log.Printf("Error from SMTP Server: %s", status)
-	}
-
-	log.Print("Email Sent Successfully")
+	err := smtp.SendMail(addr, auth, conf.FromEmail, to, msg)
+	return err
 }
 
 // ReadCsv accepts a file and returns its content as a multi-dimentional type
@@ -104,7 +111,14 @@ func readCsv(filename string) ([][]string, error) {
 		return nil, err
 	}
 
+	// Scrap the keys
 	return lines[1:], nil
+}
+
+type person struct {
+	Name    string
+	Email   string
+	Address string
 }
 
 func readPersonCsv(filename string) ([]person, error) {
@@ -116,7 +130,6 @@ func readPersonCsv(filename string) ([]person, error) {
 
 	people := make([]person, 0)
 
-	// Loop through lines & turn into object
 	for _, line := range lines {
 		people = append(people, person{
 			Name:    line[0],
@@ -150,9 +163,32 @@ func main() {
 
 	email := ""    // TODO: get from CLI
 	password := "" // TODO: Get from CLI securely
+	subject := "Secret Hanukkah Gift Exchange"
 
 	for fromPerson, toPerson := range assignReceipients(people) {
+		body, err := parseTemplate(emailTemplate, toPerson)
+
+		if err != nil {
+			log.Printf("Error creating email body: %s", err)
+			panic(err)
+		}
+
+		conf := emailConfig{
+			FromEmail: email,
+			Password:  password,
+			ToEmail:   fromPerson.Email,
+			Subject:   subject,
+			Body:      body,
+		}
+
 		log.Printf("%v: %v\n", fromPerson.Name, toPerson.Name)
-		sendMail(fromPerson, toPerson, email, password)
+		err = sendGMail(conf)
+
+		if err != nil {
+			log.Printf("Error sending email: %s", err)
+			panic(err)
+		}
+
+		log.Print("Email Sent Successfully")
 	}
 }
